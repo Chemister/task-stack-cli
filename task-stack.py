e0a92fb16699c277751fc554d29715a5bf2519b6
Chemister <jul.bell95@gmail.com>
2023-09-25 21:10:43 -0400

    very early wip 2

--- task-stack.py
+++ task-stack.py
@@ -1,6 +1,17 @@
 import sys
+from typing import List
+
+### todos
+# todo: define a task
+# todo: parse a new task and save it
+# todo: return list of tasks 
+# todo: modify a task's properties
+# todo: delete a task 
+# todo ### 
+
+def main(args: List[str]) -> bool:
+    NotImplemented
 
 if __name__ == "__main__":
-    print(f"Argv-name: {sys.argv[0]}")
-    print(f"Argv-rest: {sys.argv[1:]}")
+    sys.exit(main(sys.argv[1:]))
     

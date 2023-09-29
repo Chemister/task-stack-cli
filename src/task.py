6f205da8b8149b7f0a3b31f2ee9b9761b0fb7618
Chemister <jul.bell95@gmail.com>
2023-09-28 22:44:12 -0400

    Saving tasks as XML

--- src/task.py
+++ src/task.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 import datetime
+import os
 import string
 
 @dataclass
@@ -19,3 +20,48 @@ class Task:
                     self.priority == other.priority)
         
         return False
+    
+def taskToXML(task: Task) -> string:
+    return f"""
+<task>
+    <id>{task.id}</id>
+    <title>{task.title}</title>
+    <detail>{task.detail or ''}</detail>
+    <deadline>{task.deadline or ''}</deadline>
+    <priority>{task.priority}</priority>
+<task/>"""
+
+def saveTask(task: Task, savePath: string) -> bool :
+    data: string = ""
+
+    try:
+        if not os.path.exists(savePath):
+            f = open(savePath, "w")
+            f.close()
+
+        with open(savePath, "r+", encoding="utf-8") as f:
+            read_data = f.read()
+            if not "<?xml versions=\"1.0\" encoding=\"utf-8\"?>" in read_data:
+                data = "<?xml versions=\"1.0\" encoding=\"utf-8\"?>"
+            
+            data += taskToXML(task)
+            
+            if (len(data) > 0):
+                f.write(data)
+
+    except ValueError:
+        print(f"ERROR: File '{savePath}' cannot be opened due to an encoding error.")
+        return False
+    
+    except Exception as e:
+        print(e)
+        return False
+
+    return True
+
+if __name__ == "__main__":
+    tacheBidon = Task(1, "tâche bidon")
+    if(saveTask(tacheBidon, "test.xml")):
+        print("Succès!")
+    else:
+        print(":(")
--- task-stack.py
+++ task-stack.py
@@ -2,11 +2,17 @@ import sys
 from typing import List
 
 ### todos
-# todo: define a task
-# todo: parse a new task and save it
+# todo: define a task - ok
+# todo: figure out save file format - xml
+# todo: create a new task and save it
+# todo: handle list saving
+# todo: handle creating new folders
 # todo: return list of tasks 
 # todo: modify a task's properties
-# todo: delete a task 
+# todo: delete a task
+# todo: create a main interactive command line application
+# todo: add a function to change sorting algorithm
+# todo: add a function to CLI to return sorted list of tasks using custom algorithm  
 # todo ### 
 
 def main(args: List[str]) -> bool:
